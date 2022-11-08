45b15328ee57200c56fbce3e4340de5f8c72854f
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-11-07 22:13:49 -0600

    Creating first server with nodejs and write message as a response

---
+++ README.md
@@ -0,0 +1,10 @@
+# Web Server Using NodeJs
+- Web Server to serve static files.
+
+```
+To start project run:
+npm i
+npm run dev
+```
+Documentation references:
+[Node JS - HTTP API](https://nodejs.org/dist/latest-v18.x/docs/api/http.html)
--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hola mundo")
+const http = require('http');
+
+
+const server = http.createServer( (req, res ) => {
+    res.write('Hola mundo');
+
+    res.end();
+})
+
+server.listen( 8080 );
+
+console.log('Escuchando el puerto', 8080);
