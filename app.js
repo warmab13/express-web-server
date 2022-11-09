2d3a1524242d642a60ed8c1a08eb9f499015c45f
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-11-09 00:02:54 -0600

    Making separates routes with response

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('Hello world');
+    res.send('Home page');
+})
+
+app.get('/hola-mundo', (req, res)=>{
+    res.send('Hola mundo en su respectiva ruta');
+})
+
+app.get('*', (req, res)=>{
+    res.send('404 | page not found');
 })
 
 app.listen(8080);
