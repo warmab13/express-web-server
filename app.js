4644a31acc811e60e8bd29f10a026efea227b9f8
alonsosjwb <alonsosalcido@canplast.com.mx>
2022-11-09 01:01:30 -0600

    using parameters to show on view

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ app.set('view engine', 'hbs');
 app.use( express.static('public') );
 
 app.get('/', (req, res)=>{
-    res.render('home');
+    res.render('home', {
+        name: 'Alonso',
+        title: 'Node Workshop'
+    });
 })
 
 app.get('/portfolio', (req, res)=>{
